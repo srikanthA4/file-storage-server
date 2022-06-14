03b4f2036e81ffcde1f682500230b115413744b3
srikanth <94035252+srikanthA4@users.noreply.github.com>
2022-06-15 03:33:15 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const authRouter = require('./routes/auth.routes');
+const fileUpLoad = require('express-fileupload');
+const fileRouter = require('./routes/file.routes');
+// const corsMiddleware = require('./middleware/cors.middleware');
+const cors = require('cors');
+const filePathMiddleware = require('./middleware/filePath.middleware');
+const path = require('path');
+
+const app = express()
+const PORT = process.env.PORT || config.get('serverPort')
+
+app.use(filePathMiddleware(path.resolve(__dirname, 'files')))
+app.use(fileUpLoad([]))
+app.use(cors())
+app.use(express.json())
+app.use(express.static('static'))
+app.use('/api/auth', authRouter)
+app.use('/api/files', fileRouter)
+
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.DBURL || config.get("dbUrl"))
+        app.listen(PORT, () => {
+            console.log(`Server started on ${PORT} PORT` )
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
+
+}
+start ()
---
+++ middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = (req, res, next) => {
+    if(req.method === 'OPTIONS') {
+         next()
+        return
+    }
+
+    try{
+        const token = (req.headers.authorization || Object.values(req.headers).find(h=> h.slice?.(0, 6) === 'Bearer'))?.split(' ')[1]
+        // console.log(Object.values(req.headers).find(h=> h.slice?.(0, 6) === 'Bearer'))
+        if(!token) {
+            return  res.status(401).json({message: 'Auth error1'})
+        }
+
+        const decoded = jwt.verify(token, config.get('secretKey'))
+        req.user = decoded
+        next()
+    }
+    catch (e) {
+        return  res.status(401).json({message: 'Auth error2'})
+    }
+}
